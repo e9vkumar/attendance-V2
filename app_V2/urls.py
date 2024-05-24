819bdfd0ed2240e20cc4a06b56c4a184a6567755
vkEnine <vkumar@enine.dev>
2024-05-24 18:40:29 +0530

    new home view

--- app_V2/urls.py
+++ app_V2/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import Mainview
 
+
+app_name = "app_V2"
+
 urlpatterns = [
     path('home/',Mainview.as_view(),name="home"),
+    path('edit/',Mainview.editEntries,name="edit"),
 ]
--- app_V2/views.py
+++ app_V2/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 import datetime
-from .models import AttendanceRecord,userModel
+from .models import AttendanceRecord,userModel,statusclass
 
 # Create your views here.
 
@@ -25,25 +25,37 @@ class Mainview(TemplateView):
         # data = AttendanceRecord.objects.filter(date__lte=upper_range,date__gte=lower_range).values()
         names = userModel.objects.all().distinct()
         data = []
+        options = statusclass.objects.all().distinct()
         for name in names:
             res = AttendanceRecord.objects.filter(name_id=name.id,date__lte=upper_range,date__gte=lower_range).order_by("date")
-            past_dates,future_dates = [i.status.status for i in res if i.date < today],[i.status.status for i in res if i.date > today]
-            print("past dates",past_dates)
-            print("future dates",future_dates)
-            data.append({"employee":name,"today":today,"past_status":past_dates,"future_status":future_dates})
+            past_dates,future_dates = [{"date":i.date,"status":i.status.status} for i in res if i.date < today],[{"date":i.date,"status":i.status.status} for i in res if i.date > today]
+            # print("past dates",past_dates)
+            # print("future dates",future_dates)
+            today_status = None
+            for i in res:
+                if i.date == today:
+                    today_status = i.status.status
+
+            data.append({"employee":name,"today":today_status,"past_status":past_dates,"future_status":future_dates})
             
-        print(names)
+        # print(names)
         context = {
-        "past_dates":[ datetime.date.today() - datetime.timedelta(days=i) for i in range(1,6)] ,
+        "past_dates":[ datetime.date.today() - datetime.timedelta(days=i) for i in range(5,0,-1)] ,
         "today":datetime.date.today(),
         "future_dates":[ datetime.date.today() + datetime.timedelta(days=i) for i in range(1,6)],
-        "data":data
+        "data":data,
+        "options":options
         }
-        print(data)
+        # print(data)
         return context
     
 
-    # def editEntries(request):
+    def editEntries(request):
+        if request.method == "POST":
+            name,date,status = request.POST.get("name"),request.POST.get("date"),request.POST.get("status")
+            record = AttendanceRecord.objects.get(name=name,date=date)
+            print(record) 
+            return redirect("app_V2:home")
 
 
     
